a9bb6928055b6436b44022b2c19539d6d39618f3
elro77 <elroye77@gmail.com>
2020-05-21 21:14:37 +0300

    spaces

--- SqlProjecton/src/sql/GenerateTables.java
+++ SqlProjecton/src/sql/GenerateTables.java
@@ -58,17 +58,23 @@ public class GenerateTables { // creating the tables if they are not exists
 
 	public void generateUser(Connection con) {
 		String tableName = "user";
-		String values = "( " + "username varchar(32) NOT NULL ," + " password varchar(32) NOT NULL ,"
-				+ " connected varchar(1) NOT NULL ," + " firstName varchar(32) NOT NULL ,"
-				+ " surname varchar(32) NOT NULL ," + "email varchar(32) NOT NULL ," + "PRIMARY KEY (username) )";
+		String values = "( " + "username varchar(32) NOT NULL ,"
+		        + " password varchar(32) NOT NULL ,"
+				+ " connected varchar(1) NOT NULL ," 
+		        + " firstName varchar(32) NOT NULL ,"
+				+ " surname varchar(32) NOT NULL ,"
+		        + "email varchar(32) NOT NULL ," + "PRIMARY KEY (username) )";
 		generateTable(con, tableName, values);
 	}
 
 	public void generateEmployee(Connection con) {
 		String tableName = "employee";
-		String values = "( " + "employeeID int NOT NULL AUTO_INCREMENT ," + " role varchar(32) NOT NULL , "
-				+ " affiliation varchar(32) NOT NULL , " + " fkUsername varchar(32) NOT NULL , "
-				+ " PRIMARY KEY (employeeID) ," + "KEY employee_ibfk_1 (fkUsername), "
+		String values = "( " + "employeeID int NOT NULL AUTO_INCREMENT ," 
+		        + " role varchar(32) NOT NULL , "
+				+ " affiliation varchar(32) NOT NULL , " 
+		        + " fkUsername varchar(32) NOT NULL , "
+				+ " PRIMARY KEY (employeeID) ," 
+		        + "KEY employee_ibfk_1 (fkUsername), "
 				+ " CONSTRAINT employee_ibfk_1 FOREIGN KEY (fkUsername) "
 				+ "REFERENCES user (username) ON DELETE CASCADE ON UPDATE CASCADE )";
 		generateTable(con, tableName, values);
@@ -76,17 +82,22 @@ public class GenerateTables { // creating the tables if they are not exists
 
 	public void generateCustomer(Connection con) {
 		String tableName = "customer";
-		String values = "( " + "customerID varchar(32) NOT NULL ," + " creditCard varchar(32) NOT NULL ,"
-				+ " fkUsername varchar(32) NOT NULL ," + " PRIMARY KEY (customerID) ,"
-				+ " KEY customer_ibfk_1 (fkUsername) ," + " CONSTRAINT customer_ibfk_1 FOREIGN KEY (fkUsername) "
+		String values = "( " + "customerID varchar(32) NOT NULL ,"
+				+ " creditCard varchar(32) NOT NULL ,"
+				+ " fkUsername varchar(32) NOT NULL ," 
+				+ " PRIMARY KEY (customerID) ,"
+				+ " KEY customer_ibfk_1 (fkUsername) ," 
+				+ " CONSTRAINT customer_ibfk_1 FOREIGN KEY (fkUsername) "
 				+ " REFERENCES user (username) ON DELETE CASCADE ON UPDATE CASCADE )";
 		generateTable(con, tableName, values);
 	}
 	
 	public void generateSalePattern(Connection con) {	
 		String tableName = "sales_pattern";
-		String values = "( " + "salesPatternID INT NOT NULL AUTO_INCREMENT ," + " startTime varchar(32) NOT NULL ,"
-				+ " endTime varchar(32) NOT NULL ," + " PRIMARY KEY (salesPatternID) )";
+		String values = "( " + "salesPatternID INT NOT NULL AUTO_INCREMENT ," 
+				+ " startTime varchar(32) NOT NULL ,"
+				+ " endTime varchar(32) NOT NULL ," 
+				+ " PRIMARY KEY (salesPatternID) )";
 		generateTable(con, tableName, values);
 	}
 
